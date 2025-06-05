145d40fbb5085e8df8065eb4250c59ac412d4e6b
Zeeshan Shakeel <zee.shakeel@gmail.com>
2025-06-05 11:46:28 +0500

    hard coded jsons and directory view segue

--- EmployeeDirectoryApp/EmployeeDirectoryApp/Common/AppConstants.m
+++ EmployeeDirectoryApp/EmployeeDirectoryApp/Common/AppConstants.m
@@ -8,9 +8,9 @@
 #import "AppConstants.h"
 
 // API Urls
-NSString *const kEmployeeAPIURLEmployees = @"https://s3.amazonaws.com/sq-mobile-interview/employees.json";
-NSString *const kEmployeeAPIURLEmptyList = @"https://s3.amazonaws.com/sq-mobile-interview/employees_empty.json";
-NSString *const kEmployeeAPIURLMalformed = @"https://s3.amazonaws.com/sq-mobile-interview/employees_malformed.json";
+NSString *const kEmployeeAPIURLEmployees = @"https://dummyjson.com/c/9bb7-7c58-4a98-93a7"; //@"https://s3.amazonaws.com/sq-mobile-interview/employees.json";
+NSString *const kEmployeeAPIURLEmptyList = @"https://dummyjson.com/c/f54b-e6d1-4835-8a0f"; //@"https://s3.amazonaws.com/sq-mobile-interview/employees_empty.json";
+NSString *const kEmployeeAPIURLMalformed = @"https://dummyjson.com/c/850c-1043-4724-b862"; //@"https://s3.amazonaws.com/sq-mobile-interview/employees_malformed.json";
 NSString *const kEmployeeAPIBaseURL = kEmployeeAPIURLEmployees;
 
 // Segues
